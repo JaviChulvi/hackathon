fc3786528183fba3199ff71a9c67d685706b527d
JaviChulvi <al364498@uji.es>
2020-02-17 13:54:34 +0000

    practice done

--- hashcode/practice/main.py
+++ hashcode/practice/main.py
@@ -12,40 +12,55 @@ def read_input(input_file):
         n_slices_pizza = [int(i) for i in sl_data] 
 
         return max_slices, n_types_pizza, n_slices_pizza
+def write_output(input_file, n_types_pizza, max_sublist):
+    f = open(input_file[:-2]+'out',"w")
+    f.write(str(n_types_pizza)+ '\n')
+    sublist = ''
+    for i in max_sublist:
+        sublist += str(i) + ' '
+    print(sublist)
+    f.write(sublist)
+    f.close()
+
 
 
 def order(max_slices, max_types_pizza, n_slices_pizza):
     # as many pizza slices as possible, but not more than the maximum number
-    sorted_index = sorted(range(len(n_slices_pizza)), key=lambda k: -n_slices_pizza[k]) # devuelve indices de las pizzas odenadas de mayor a menor por número de trozos
+    sorted_index = sorted(range(len(n_slices_pizza)), key=lambda k: -n_slices_pizza[k]) 
     max_sum = 0
     max_sublist = []
     max_n_types_pizza = 0
     for i in sorted_index:
-        print(n_slices_pizza[i], i)
+
         sum = n_slices_pizza[i]
         sublist = [i]
         types_pizza = 1
         elements = sorted_index
         elements.remove(i)
-        print(elements)
+
         for j in elements:
             if sum + n_slices_pizza[j] <= max_slices and types_pizza + 1 <= max_types_pizza:
                 sum = sum + n_slices_pizza[j]
                 types_pizza += 1
                 sublist.append(j)
-                print('SUM: ', sum, types_pizza, sublist)
+
         if sum > max_sum:
             max_sum = sum
             max_n_types_pizza = types_pizza
             max_sublist = sublist
-            print('MAX: ', max_sum, max_n_types_pizza, max_sublist)
-            for i in max_sublist:
-                print(n_slices_pizza[i])
+
+            if max_sum == max_slices:
+                return max_sum, max_n_types_pizza, max_sublist
+    return max_sum, max_n_types_pizza, max_sublist
 
     
 
 if __name__ == '__main__':
     input_file = sys.argv[1]
     max_slices, n_types_pizza, n_slices_pizza = read_input(input_file)
-    order(max_slices, n_types_pizza, n_slices_pizza)
+    print('MAX SLICES: ',max_slices,'MAX TYPES PIZZAS: ', n_types_pizza)
+    max_sum, max_n_types_pizza, max_sublist = order(max_slices, n_types_pizza, n_slices_pizza)
+    print('MAX SUM SLICES: ', max_sum, 'TYPES OF PIZZA USED: ',max_n_types_pizza)
+    max_sublist.sort()
+    write_output(input_file, max_n_types_pizza, max_sublist)
 
